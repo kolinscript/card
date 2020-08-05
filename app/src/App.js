c03e1b4ea61d76083f92e41289d421a82a11ed78
nkoshkarov <koshkarovnik@gmail.com>
2020-08-05 19:00:45 +0300

    add canvas background

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
 import './App.css';
+import {isNil} from "lodash";
+import ReactPageScroller from "react-page-scroller";
+import {updateCanvas} from "./misc";
 
 class App extends Component {
+    drawBackground = updateCanvas;
+
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            currentPage: null
+        };
         this.handleLoad = this.handleLoad.bind(this);
-        this.updateCanvas = this.updateCanvas.bind(this);
+        this.drawBackground = this.drawBackground.bind(this);
+        this.contentRef = React.createRef();
     }
 
     componentDidMount() {
         window.addEventListener('load', this.handleLoad);
-        this.updateCanvas();
+        this.drawBackground();
     }
 
     componentWillUnmount() {
@@ -23,205 +31,104 @@ class App extends Component {
         console.log(url_current);
     }
 
-    updateCanvas() {
-        const {PI, cos, sin, abs, random} = Math;
-        const TAU = 2 * PI;
-        const rand = n => n * random();
-        const fadeInOut = (t, m) => {
-            let hm = 0.5 * m;
-            return abs((t + hm) % m - hm) / (hm);
-        };
-        const circleCount = 150;
-        const circlePropCount = 8;
-        const circlePropsLength = circleCount * circlePropCount;
-        const baseSpeed = 0.1;
-        const rangeSpeed = 1;
-        const baseTTL = 150;
-        const rangeTTL = 200;
-        const baseRadius = 100;
-        const rangeRadius = 200;
-        const rangeHue = 60;
-        const xOff = 0.0015;
-        const yOff = 0.0015;
-        const zOff = 0.0015;
-        const backgroundColor = 'hsla(0,0%,5%,1)';
-        let ctx;
-        let container;
-        let canvas;
-        let circleProps;
-        let simplex;
-        let baseHue;
+    handlePageChange = number => {
+        this.setState({ currentPage: number }); // set currentPage number, to reset it from the previous selected.
+    };
 
-        const setup = () => {
-            createCanvas();
-            resize();
-            initCircles();
-            draw();
-        }
-
-        const initCircles = () => {
-            circleProps = new Float32Array(circlePropsLength);
-            simplex = new window.SimplexNoise();
-            baseHue = 220;
-
-            let i;
-
-            for (i = 0; i < circlePropsLength; i += circlePropCount) {
-                initCircle(i);
+    wheel(event) {
+        if (this.state.scrollerIndex >= 0) {
+            if (event.deltaY < 0) {
+                // scrollWindowUp();
+                if (!this.state.scrollerActing) {
+                    console.info('SCROLLUP ' + event.deltaY);
+                    this.setState({scrollerIndex: this.state.scrollerIndex + 1, scrollerActing: true}, () => {
+                        this.contentRef.current.style.transform = `translate3d(0, ${(this.state.scrollerIndex) * -100}%, 0)`;
+                        console.log('scrollerIndex ', this.state.scrollerIndex);
+                        console.log('scrollerActing ', this.state.scrollerActing);
+                        setTimeout(() => {
+                            this.setState({scrollerActing: false});
+                        }, 2000);
+                    });
+                }
+            } else {
+                // scrollWindowDown;
+                if (!this.state.scrollerActing) {
+                    console.info('SCROLLDOWN ' + event.deltaY);
+                    if (this.state.scrollerIndex === 0) {
+                        this.setState({scrollerIndex: this.state.scrollerIndex, scrollerActing: true}, () => {
+                            this.contentRef.current.style.transform = `translate3d(0, 0, 0)`;
+                            console.log('scrollerIndex ', this.state.scrollerIndex);
+                            console.log('scrollerActing ', this.state.scrollerActing);
+                            setTimeout(() => {
+                                this.setState({scrollerActing: false});
+                            }, 2000);
+                        });
+                    } else {
+                        this.setState({scrollerIndex: this.state.scrollerIndex - 1, scrollerActing: true}, () => {
+                            this.contentRef.current.style.transform = `translate3d(0, ${(this.state.scrollerIndex) * 100}%, 0)`;
+                            console.log('scrollerIndex ', this.state.scrollerIndex);
+                            console.log('scrollerActing ', this.state.scrollerActing);
+                            setTimeout(() => {
+                                this.setState({scrollerActing: false});
+                            }, 2000);
+                        });
+                    }
+                }
             }
         }
-
-        const initCircle = (i) => {
-            let x, y, n, t, speed, vx, vy, life, ttl, radius, hue;
-
-            x = rand(canvas.a.width);
-            y = rand(canvas.a.height);
-            n = simplex.noise3D(x * xOff, y * yOff, baseHue * zOff);
-            t = rand(TAU);
-            speed = baseSpeed + rand(rangeSpeed);
-            vx = speed * cos(t);
-            vy = speed * sin(t);
-            life = 0;
-            ttl = baseTTL + rand(rangeTTL);
-            radius = baseRadius + rand(rangeRadius);
-            hue = baseHue + n * rangeHue;
-
-            circleProps.set([x, y, vx, vy, life, ttl, radius, hue], i);
-        }
-
-        const updateCircles = () => {
-            let i;
-            baseHue++;
-            for (i = 0; i < circlePropsLength; i += circlePropCount) {
-                updateCircle(i);
-            }
-        }
-
-        const updateCircle = (i) => {
-            let i2 = 1 + i, i3 = 2 + i, i4 = 3 + i, i5 = 4 + i, i6 = 5 + i, i7 = 6 + i, i8 = 7 + i;
-            let x, y, vx, vy, life, ttl, radius, hue;
-
-            x = circleProps[i];
-            y = circleProps[i2];
-            vx = circleProps[i3];
-            vy = circleProps[i4];
-            life = circleProps[i5];
-            ttl = circleProps[i6];
-            radius = circleProps[i7];
-            hue = circleProps[i8];
-
-            drawCircle(x, y, life, ttl, radius, hue);
-
-            life++;
-
-            circleProps[i] = x + vx;
-            circleProps[i2] = y + vy;
-            circleProps[i5] = life;
-
-            (checkBounds(x, y, radius) || life > ttl) && initCircle(i);
-        }
-
-        const drawCircle = (x, y, life, ttl, radius, hue) => {
-            ctx.a.save();
-            ctx.a.fillStyle = `hsla(${hue},60%,30%,${fadeInOut(life, ttl)})`;
-            ctx.a.beginPath();
-            ctx.a.arc(x, y, radius, 0, TAU);
-            ctx.a.fill();
-            ctx.a.closePath();
-            ctx.a.restore();
-        }
-
-        const checkBounds = (x, y, radius) => {
-            return (
-                x < -radius ||
-                x > canvas.a.width + radius ||
-                y < -radius ||
-                y > canvas.a.height + radius
-            );
-        }
-
-        const createCanvas = () => {
-            container = document.querySelector('.wrapper-canvas');
-            canvas = {
-                a: document.createElement('canvas'),
-                b: document.createElement('canvas')
-            };
-            canvas.b.style = `
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            `;
-            container.appendChild(canvas.b);
-            ctx = {
-                a: canvas.a.getContext('2d'),
-                b: canvas.b.getContext('2d')
-            };
-        }
-
-        const resize = () => {
-            const {innerWidth, innerHeight} = window;
-
-            canvas.a.width = innerWidth;
-            canvas.a.height = innerHeight;
-
-            ctx.a.drawImage(canvas.b, 0, 0);
-
-            canvas.b.width = innerWidth;
-            canvas.b.height = innerHeight;
-
-            ctx.b.drawImage(canvas.a, 0, 0);
-        }
-
-        const render = () => {
-            ctx.b.save();
-            ctx.b.filter = 'blur(50px)';
-            ctx.b.drawImage(canvas.a, 0, 0);
-            ctx.b.restore();
-        }
-
-        const draw = () => {
-            ctx.a.clearRect(0, 0, canvas.a.width, canvas.a.height);
-            ctx.b.fillStyle = backgroundColor;
-            ctx.b.fillRect(0, 0, canvas.b.width, canvas.b.height);
-            updateCircles();
-            render();
-            window.requestAnimationFrame(draw);
-        }
-
-        window.addEventListener('load', setup);
-        window.addEventListener('resize', resize);
     }
 
     render() {
         return (
             <div className="app">
-                <section className={'section section-one'}>
+                <div className={'main'}>
                     <div className="wrapper-canvas"></div>
-                    <div className="wrapper-content">
-                        <h1>frontend engineer</h1>
-                        <h2>Nikolai Koshkarov</h2>
-                        <div className="links">
-                            <div className="link">
-                                <a className="github"
-                                   href="https://github.com/kolinscript"
-                                   target="_self"
-                                   rel="noopener noreferrer"
-                                >github
-                                </a>
-                            </div>
-                            <div className="link">
-                                <a className="telega"
-                                   href="https://t.me/kolingram"
-                                   target="_blank"
-                                   rel="noopener noreferrer"
-                                >telegram
-                                </a>
-                            </div>
+                    <div className="wrapper-content"
+                         onWheel={(e) => this.wheel(e)}
+                    >
+                        <div className="border"><span></span></div>
+                        <div className="content">
+                            <ReactPageScroller
+                                containerHeight='100%'
+                                containerWidth='100%'
+                                pageOnChange={this.handlePageChange}
+                                customPageNumber={this.state.currentPage}
+                            >
+                                <div className="item">
+                                    <h1>frontend engineer</h1>
+                                    <h2>Nikolai Koshkarov</h2>
+                                    <div className="links">
+                                        <div className="link">
+                                            <a className="github"
+                                               href="https://github.com/kolinscript"
+                                               target="_self"
+                                               rel="noopener noreferrer"
+                                            >github
+                                            </a>
+                                        </div>
+                                        <div className="link">
+                                            <a className="gmail"
+                                               mailTo="sdaasda"
+                                            >telegram
+                                            </a>
+                                        </div>
+                                        <div className="link">
+                                            <a className="telega"
+                                               href="https://t.me/kolingram"
+                                               target="_blank"
+                                               rel="noopener noreferrer"
+                                            >telegram
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item">
+                                    <h1>my works</h1>
+                                </div>
+                            </ReactPageScroller>
                         </div>
                     </div>
-                </section>
+                </div>
             </div>
         );
     }
